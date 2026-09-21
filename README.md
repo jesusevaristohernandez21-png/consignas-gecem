a6a5e6c2b862645bd376ce8ec261f2dfa883b3cd
jesusevaristohernandez21-png <jesus.evaristo.hernandez21@gmail.com>
2026-09-20 21:34:05 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Consignas GECEM
+
+App instalable (PWA) de consignas de vigilancia. El servidor es un Google Apps Script; este repo solo publica la interfaz en GitHub Pages.
---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'consignas-gecem-v2-0';
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  // El servidor (Apps Script) y las fotos de Drive nunca se cachean
+  if (url.origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(req).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {}); return res; })
+      .catch(() => caches.match(req).then(hit => hit || caches.match('./index.html')))
+  );
+});
