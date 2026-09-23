48d015655241676e7d55e4c7a8bab22607a8dd4f
jesusevaristohernandez21-png <jesus.evaristo.hernandez21@gmail.com>
2026-09-23 17:28:45 -0600

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'consignas-gecem-v2-5';
+const CACHE = 'consignas-gecem-v2-7';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
